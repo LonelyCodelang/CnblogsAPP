d827e980bafc49e871e0e734b1fe89e27bc44e28
helang <447111964@qq.com>
2016-07-06 19:00:52 +0800

    增加新闻页面

--- app/src/main/java/com/mcnblogs/Fragment/NewsFragment.java
+++ app/src/main/java/com/mcnblogs/Fragment/NewsFragment.java
@@ -3,6 +3,7 @@ package com.mcnblogs.Fragment;
 import com.google.gson.Gson;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.mcnblogs.R;
+import com.mcnblogs.adapter.NewListAdapter;
 import com.mcnblogs.core.Config;
 import com.mcnblogs.dto.NewDTO;
 import com.mcnblogs.utility.APPUtil;
@@ -15,6 +16,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import org.apache.http.Header;
 
@@ -22,54 +25,69 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NewsFragment extends Fragment {
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_news, container, false);
-		Load();
-		return view;
-	}
 
-	/**
-	 * 加载数据
-	 */
-	private  void  Load(){
-		//列表Url
-		String url= Config.ListNewsPage_URL;
-		HttpUtil.get(url, new AsyncHttpResponseHandler() {
-			@Override
-			public void onSuccess(int i, Header[] headers, byte[] bytes) {
-				if(i==200){
-					String response = new String(bytes);// 返回字符串
-					JsonAnalysis(response);
-				}else{
-					// 数据返回异常
-					APPUtil.ShowMsgNetError(getActivity());
-				}
-			}
+    private View view;
 
-			@Override
-			public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
-				// 数据返回异常
-				APPUtil.ShowMsgNetError(getActivity());
-			}
-		});
-	}
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_news, container, false);
+        Load();
+        return view;
+    }
 
-	/**
-	 * 字符串解析成json
-	 * @param strJson
-	 */
-	private  void JsonAnalysis(String strJson){
-		String resJson = XmlJSON.xml2JSON(strJson);// 将xml转换成字符串
-		try {
-			Gson gs=new Gson();
-			NewDTO dto=gs.fromJson(resJson,NewDTO.class);//序列化json
-			List<NewDTO.FeedBean.EntryBean> list= dto.getFeed().getEntry();
-			
+    /**
+     * 加载数据
+     */
+    private void Load() {
+        //列表Url
+        String url = Config.ListNewsPage_URL;
+        HttpUtil.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                if (i == 200) {
+                    String response = new String(bytes);// 返回字符串
+                    JsonAnalysis(response);
+                } else {
+                    // 数据返回异常
+                    APPUtil.ShowMsgNetError(getActivity());
+                }
+            }
 
-		}catch (Exception ex){
-			String msg=ex.getMessage();
-			Log.e("json转换错误",msg);
-		}
-	}
+            @Override
+            public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+                // 数据返回异常
+                APPUtil.ShowMsgNetError(getActivity());
+            }
+        });
+    }
+
+    /**
+     * 字符串解析成json
+     *
+     * @param strJson
+     */
+    private void JsonAnalysis(String strJson) {
+        String resJson = XmlJSON.xml2JSON(strJson);// 将xml转换成字符串
+        try {
+            Gson gs = new Gson();
+            NewDTO dto = gs.fromJson(resJson, NewDTO.class);//序列化json
+            final List<NewDTO.FeedBean.EntryBean> list = dto.getFeed().getEntry();
+
+            ListView lv = (ListView) view.findViewById(R.id.lv_newlist);
+            NewListAdapter dap = new NewListAdapter(getActivity(), R.layout.new_item, list);
+            lv.setAdapter(dap);
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    NewDTO.FeedBean.EntryBean dto = list.get(i);
+
+                    APPUtil.ShowMsg(getActivity(),dto.getTitle().getContent());
+                }
+            });
+
+        } catch (Exception ex) {
+            String msg = ex.getMessage();
+            Log.e("json转换错误", msg);
+        }
+    }
 }
---
+++ app/src/main/java/com/mcnblogs/adapter/NewListAdapter.java
@@ -0,0 +1,63 @@
+package com.mcnblogs.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mcnblogs.R;
+import com.mcnblogs.dto.NewDTO;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+
+/**
+ * 作者：hl
+ * 时间:2016/7/6 17:55
+ * 说明:新闻列表类型
+ */
+public class NewListAdapter extends ArrayAdapter<NewDTO.FeedBean.EntryBean> {
+
+    private int resourceid;
+    private View view;
+    public NewListAdapter(Context context, int resource, List<NewDTO.FeedBean.EntryBean> objects) {
+        super(context, resource, objects);
+        resourceid=resource;
+    }
+
+    public  View getView(int position, View convertView, ViewGroup parent){
+        NewDTO.FeedBean.EntryBean dto=getItem(position);
+        if(convertView==null){
+            view = LayoutInflater.from(getContext()).inflate(resourceid, null);
+        }
+        else{
+            view=convertView;
+        }
+        TextView tv=(TextView)view.findViewById(R.id.tv_title);
+        ImageView img=(ImageView)view.findViewById(R.id.img_ioc);
+
+        tv.setText(dto.getTitle().getContent());
+        SetImg(dto.getTopicIcon());
+
+        return view;
+    }
+
+    /**
+     * 加载图片
+     * @param url
+     */
+    private void SetImg(String url){
+        if(!url.isEmpty()){
+            ImageView imgv=(ImageView)view.findViewById(R.id.img_ioc);
+            Picasso.with(getContext())
+                    .load(url)
+                    .placeholder(R.drawable.ic_launcher)
+                    .error(R.drawable.ic_launcher)
+                    .into(imgv);
+        }
+    }
+}
